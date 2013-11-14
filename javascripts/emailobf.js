18ee2b8f47622a1f97925a28bd88d4aa39f6715a
abcalderon3 <abc3@abc3s-air.home>
2013-11-14 12:09:27 -0500

    Fixed jQuery

--- javascripts/emailobf.js
+++ javascripts/emailobf.js
@@ -1,9 +1,11 @@
-$('#my-email').html(function(){
-	var e = "abc3";
-	var a = "@";
-	var d = "adriancalderon";
-	var c = ".org";
-	var h = 'mailto:' + e + a + d + c;
-	$(this).parent('a').attr('href', h);
-	return e + a + d + c;
+$(document).ready(function() {
+	$("#my-email").html(function(){
+		var e = "abc3";
+		var a = "@";
+		var d = "adriancalderon";
+		var c = ".org";
+		var h = 'mailto:' + e + a + d + c;
+		$(this).parent('a').attr('href', h);
+		return e + a + d + c;
+	});
 });
